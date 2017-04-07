e92108a764fbf01324e1b0d0d4b895aea5d4c473
Ilya Nozhkin <nozhkin.ii@gmail.com>
2017-04-08 01:39:37 +0300

    relocated all text sections to irom (exlcuding call_user_start), added spi flash initialization

--- src/arch/xtensa/kernel/boot_entry.c
+++ src/arch/xtensa/kernel/boot_entry.c
@@ -11,7 +11,21 @@ void zerobss(void)
 	for (addr = &_bss_start; addr < &_bss_end; addr++)  *addr = 0; //Safe, _bss_start doesn't have to == _bss_end
 }
 
-void call_user_start(void) {
+extern void SPIUnlock(void);
+extern void Cache_Read_Enable(uint32_t, uint32_t, uint32_t);
+
+#define GPIO0_MUX_alt (*((volatile uint32_t*) 0x60000834))
+#define GPIO_MUX_CFG_alt (*((volatile uint32_t*) 0x60000800))
+#define SPI0_USER (*((volatile uint32_t*) 0x6000021C))
+#define SPI0_CTRL (*((volatile uint32_t*) 0x60000208))
+
+void __attribute__((section(".spec.call_user_start"))) call_user_start(void) {
+	GPIO0_MUX_alt = 1 << 7;
+	SPI0_USER |= 1 << 5;
+	GPIO_MUX_CFG_alt &= ~(1 << 8);
+	SPI0_CTRL = (SPI0_CTRL & (~0x1FFF)) | 0x0101;
+	Cache_Read_Enable(0, 0, 1);
+	
 	zerobss();
 	kernel_start();
 }
