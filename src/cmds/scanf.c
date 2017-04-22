46a0a5a9786c3d57ff825510b8275cbe639393de
Victor Y. Fadeev <victor.y.fadeev@gmail.com>
2017-04-22 12:07:54 +0300

    Start work with scanf()

---
+++ src/cmds/scanf.c
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @brief Test scanf function.
+ *
+ * @date 22.04.17
+ * @author Victor Y. Fadeev
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	char string[256];
+
+    scanf("%s", string);
+    printf("Your sting - %s" ,string);
+
+	return 0;
+}
+ 
