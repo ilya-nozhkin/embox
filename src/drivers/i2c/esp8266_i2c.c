5e9c30b8d7773804017fee03e53b568175f65d36
denis417 <denis417k@gmail.com>
2017-06-30 14:54:12 +0300

    i2c receive function implemented

--- src/drivers/i2c/esp8266_i2c.c
+++ src/drivers/i2c/esp8266_i2c.c
@@ -4,6 +4,11 @@
 
 #include <embox/unit.h>
 
+#define DELAY
+
+strcut gpio* SDA;
+strcut gpio* SCL;
+
 void i2c_start(void) {
 }
 
@@ -14,4 +19,54 @@ uint8_t i2c_send(uint8_t data) {
 }
 
 uint8_t i2c_receive(uint8_t last) {
+
+	lineDown (SCL);
+
+	uint8_t bite=0;
+	struct gpio_mask_t SDAlevel;
+
+	for (unsigned int i = 0; i < 8; i++ )
+	{
+		bite = bite<<1;
+		delay();
+		lineUp(SCL);
+		SDAlevel = gpio_get_level(SDA, 0);
+
+		if (SDAlevel == 1)
+		{
+			bite = bite | 1;
+		}
+
+		lineDown (SCL);
+	}
+
+	delay();
+
+	if (last == 0)
+	{
+		lineDown(SDA);
+		lineUp(SCL);
+		delay();
+		lineDown(SCL);
+		lineUp(SDA);
+	}
+	else
+	{
+		lineUp(SCL);
+		delay();
+		lineDown(SCL);
+	}
+
+
+	return bite;
+}
+
+static inline void lineDown (strcut gpio* gpio)
+{
+	gpio_settings(gpio, 0, GPIO_MODE_OUTPUT);
+}
+
+static inline void lineUp (strcut gpio* gpio)
+{
+	gpio_settings(gpio, 0, GPIO_MODE_INPUT);
 }
