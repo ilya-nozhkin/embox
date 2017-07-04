d6031d1ad818c730c480aa1ee83d00dba950af22
Ilya Nozhkin <nozhkin.ii@gmail.com>
2017-07-04 11:43:57 +0300

    implemented l298p driver

--- src/cmds/servo.c
+++ src/cmds/servo.c
@@ -4,7 +4,21 @@
 #include <drivers/servo.h>
 
 int main(int argc, char **argv) {
-	servo_enable(servo_by_id(0));
+	if (argc >= 3) {
+		int id = argv[2][0] - '0';
+		if (argv[1][0] == 'e') {
+			servo_enable(servo_by_id(id));
+		}
+		if (argv[1][0] == 'd') {
+			servo_disable(servo_by_id(id));
+		}
+		if (argv[1][0] == 'f') {
+			servo_set_direction(servo_by_id(id), 1);
+		}
+		if (argv[1][0] == 'b') {
+			servo_set_direction(servo_by_id(id), 0);
+		}
+	}
 
 	return 0;
 }
--- src/drivers/servo/l298p.c
+++ src/drivers/servo/l298p.c
@@ -13,32 +13,32 @@
 struct servo motorA = {.id = 0};
 struct servo motorB = {.id = 1};
 
+#define PWMA 5
+#define PWMB 12
+#define DIRA 4
+#define DIRB 13
+
 struct servo *servo_by_id(uint32_t motor_id) {
 	return motor_id == 0 ? &motorA : &motorB;
 }
 
 void servo_enable(struct servo *motor) {
-	if (motor->id == 0) {
-		struct gpio *pwmA = gpio_by_num(5);
-		gpio_settings(pwmA, 0, GPIO_MODE_OUTPUT);
-		gpio_set_level(pwmA, 0, 1);
-		
-		struct gpio *pwmB = gpio_by_num(12);
-		gpio_settings(pwmB, 0, GPIO_MODE_OUTPUT);
-		gpio_set_level(pwmB, 0, 1);
-		
-		struct gpio *dirA = gpio_by_num(4);
-		gpio_settings(dirA, 0, GPIO_MODE_OUTPUT);
-		gpio_set_level(dirA, 0, 1);
-		
-		struct gpio *dirB = gpio_by_num(13);
-		gpio_settings(dirB, 0, GPIO_MODE_OUTPUT);
-		gpio_set_level(dirB, 0, 1);
-	}
+	struct gpio *pwm = gpio_by_num(motor->id == 0 ? PWMA : PWMB);
+
+	gpio_settings(pwm, 0, GPIO_MODE_OUTPUT);
+	gpio_set_level(pwm, 0, 1);
 }
 
 void servo_disable(struct servo *motor) {
+	struct gpio *pwm = gpio_by_num(motor->id == 0 ? PWMA : PWMB);
+
+	gpio_settings(pwm, 0, GPIO_MODE_OUTPUT);
+	gpio_set_level(pwm, 0, 0);
 }
 
 void servo_set_direction(struct servo *motor, uint32_t direction) {
+	struct gpio *dir = gpio_by_num(motor->id == 0 ? DIRA : DIRB);
+
+	gpio_settings(dir, 0, GPIO_MODE_OUTPUT);
+	gpio_set_level(dir, 0, direction);
 }
