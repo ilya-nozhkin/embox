a8d5e4902714c38d6ca70924db3f3932874001d4
Egor Shitov <liberaid2@gmail.com>
2017-07-13 13:05:53 +0300

    ruc cmd: file path as a parameter

--- src/cmds/ruc.c
+++ src/cmds/ruc.c
@@ -12,8 +12,17 @@
 
 extern int ruc_import();
 
-int main(int argc, char *argv[]) {
-	ruc_import("/export.txt");
+int main(int argc, char **argv) {
+
+	char dir[] = "/flash/";
+	char *path = malloc(strlen(argv[1]) + 1 + 7);
+
+	memcpy(path, dir, 7);
+	memcpy(path+7, argv[1], strlen(argv[1]) + 1);
+
+	ruc_import(path);
+
 	fflush(stdout);
+	free(path);
 	return 0;
 }
