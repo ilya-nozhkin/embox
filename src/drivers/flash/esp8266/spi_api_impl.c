e83adc0e495542267b30f3314cdf536aca6ee7c5
Egor Shitov <liberaid2@gmail.com>
2017-07-13 16:37:38 +0300

    esp8266 flash: works after reset

--- src/drivers/flash/esp8266/spi_api_impl.c
+++ src/drivers/flash/esp8266/spi_api_impl.c
@@ -8,6 +8,10 @@
 
 #include "spi_api_impl.h"
 
+#include <embox/unit.h>
+#include <kernel/printk.h>
+#include <hal/reg.h>
+
 static char buffer[FLASH_BLOCK_SIZE];
 
 // Let's make 'addr' is for converted addr, '_addr' is for not
@@ -20,7 +24,9 @@ static inline uint8_t is_oversize(uint32_t _addr, uint32_t size){
 }
 
 SpiFlashOpResult spi_flash_erase_sector(uint16_t sector){
+	Cache_Read_Disable();
 	SpiFlashOpResult result = SPIEraseSector(sector + MIN_SECTOR_NUMBER);
+	Cache_Read_Enable(0, 0, 1);
 	return result;
 }
 
@@ -52,7 +58,11 @@ SpiFlashOpResult spi_flash_write(uint32_t dest_addr, uint32_t *src_addr, uint32_
 		if(is_oversize(_dest_addr, size))
 			return SPI_FLASH_RESULT_OVERSIZE;
 
-		return SPIWrite(_dest_addr, src_addr, size);
+		Cache_Read_Disable();
+		Wait_SPI_Idle(flashchip);
+		SpiFlashOpResult res =  SPIWrite(_dest_addr, src_addr, size);
+		Cache_Read_Enable(0,0,1);
+		return res;
 	}
 
 }
@@ -66,5 +76,23 @@ SpiFlashOpResult spi_flash_read(uint32_t src_addr, uint32_t* dest_addr, uint32_t
 	if(is_oversize(_src_addr, size))
 		return SPI_FLASH_RESULT_OVERSIZE;
 
-	return SPIRead(_src_addr, dest_addr, size);
+	Cache_Read_Disable();
+	Wait_SPI_Idle(flashchip);
+	SpiFlashOpResult res = SPIRead(_src_addr, dest_addr, size);
+	Cache_Read_Enable(0,0,1);
+	return res;
+}
+
+EMBOX_UNIT_INIT(esp8266_spi_setup);
+static int esp8266_spi_setup(void){
+
+	printk("deviceId: %u\nchip_size: %u\nblock_size: %u\nsector_size: %u\npage_size: %u\nstatus_mask: %u\n",
+		flashchip->deviceId,
+		flashchip->chip_size,
+		flashchip->block_size,
+		flashchip->sector_size,
+		flashchip->page_size,
+		flashchip->status_mask);
+
+	return 0;
 }
--- src/drivers/flash/esp8266/spi_api_impl.h
+++ src/drivers/flash/esp8266/spi_api_impl.h
@@ -12,9 +12,24 @@
 #define FLASH_BLOCK_SIZE 	16				// block size (bytes)
 #define FLASH_SECTOR_SIZE   (4*1024)		// sector size 4KBytes (esp8266-documented)
 #define FLASH_MAX_SIZE      (2*1024*1024)	// actually 4MBytes (esp8266-documented)
-#define MIN_SECTOR_NUMBER   16				// reserved for executable code
+#define MIN_SECTOR_NUMBER   32				// reserved for executable code
+
+#define IDX_SPI_CMD 0
+#define IDX_SPI_ADDR 1
+
+#define SPI0_CMD spi0_[IDX_SPI_CMD]
+#define SPI0_ADDR spi0_[IDX_SPI_ADDR]
 
 #include <stdint.h>
+
+typedef struct {
+	uint32_t deviceId;
+	uint32_t chip_size;
+	uint32_t block_size;
+	uint32_t sector_size;
+	uint32_t page_size;
+	uint32_t status_mask;
+} SpiFlashChip;							// esp8266-documented
 
 typedef enum{
 	SPI_FLASH_RESULT_OK,				// esp8266-documented
@@ -24,10 +39,17 @@ typedef enum{
 	SPI_FLASH_RESULT_OVERSIZE			// happens when addr starts in one sector and addr+size ends in another one
 } SpiFlashOpResult;
 
+extern volatile uint32_t spi0_[64];
+
 /* built-in esp8266 functions */
+extern void Wait_SPI_Idle(SpiFlashChip*);
+extern void Cache_Read_Disable(void);
+extern void Cache_Read_Enable(uint32_t,uint32_t,uint32_t);
 extern SpiFlashOpResult SPIEraseSector(uint16_t sector);
 extern SpiFlashOpResult SPIWrite(uint32_t dest_addr, uint32_t *src_addr, uint32_t size);
 extern SpiFlashOpResult SPIRead(uint32_t src_addr, uint32_t *dest_addr, uint32_t size);
+/* built-in esp8266 struct */
+extern SpiFlashChip *flashchip;
 
 /* documented esp8266 functions */
 // Doesn't work at all
