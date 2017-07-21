2f8369d847d17827f9993c494cfdeb87cb38289d
Egor Shitov <liberaid2@gmail.com>
2017-07-21 12:34:40 +0300

    pwm: added interface

--- src/cmds/flashdisk_create.c
+++ src/cmds/flashdisk_create.c
@@ -2,34 +2,28 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <drivers/block_dev/esp8266/flashdisk.h>
-#include <drivers/pwm/lx106/pwm.h>
+#include <drivers/pwm/pwm.h>
 
 static int ok = 0;
+static int fid = 0;
+static int sid = 0;
 
 int main(int argc, char **argv) {
 
 	//flashdisk_create(argv[1], 64*1024);
 
-	lx106_pwm_t first;
-	lx106_pwm_t second;
+	if(!ok) {
+		fid = pwm_add(5);
+		sid = pwm_add(12);
+	}
 
-	first.pin = 5;
-	second.pin = 12;
-
-	first.power = atoi(argv[1]);
-	second.power = atoi(argv[2]);
-
-	int f = lx106_pwm_add_servo(0, first);
-	int s = lx106_pwm_add_servo(1, second);
-
-	printf("First: %d second: %d\n", f, s);
-
-	if(ok)
-		return 0;
-
-	int res = lx106_pwm_enable();
-	printf("Enabled: %d\n", res);
 	ok++;
+
+	pwm_set_power(fid, atoi(argv[1]));
+	pwm_set_power(sid, atoi(argv[2]));
+
+	if(ok++ == 1)
+		pwm_enable();
 
 	return 0;
 }
--- src/drivers/pwm/lx106/pwm.c
+++ src/drivers/pwm/lx106/pwm.c
@@ -1,4 +1,4 @@
-#include "pwm.h"
+#include <drivers/pwm/pwm.h>
 
 #include <hal/reg.h>
 #include <kernel/irq.h>
@@ -9,7 +9,6 @@
 
 static uint32_t ccount = 0;
 static uint8_t stage = 0; // current stage in percents
-static lx106_pwm_t servos[MAX_SERVOS] = {0};
 
 static irq_return_t lx106_pwm_handle(int irq_nr, void *data);
 
@@ -18,29 +17,23 @@ static inline void change_ccompare(void){
     SREG_WRITE("ccompare0", ccount + CCOMPARE_OFFSET);
 }
 
-int lx106_pwm_enable(void){
+int pwm_enable(void){
     int res = irq_attach(IRQ_NR, lx106_pwm_handle, 0, NULL, "lx106 pwm handler");
     irqctrl_enable(IRQ_NR);
     change_ccompare();
     return res;
 }
 
-int lx106_pwm_disable(void){
+int pwm_disable(void){
     irqctrl_disable(IRQ_NR);
     return irq_detach(IRQ_NR, NULL);
 }
 
 static irq_return_t lx106_pwm_handle(int irq_nr, void *data){
-    for(uint8_t i = 0; i < MAX_SERVOS; i++){
-        lx106_pwm_t *servo = &servos[i];
+    for(uint8_t i = 0; i < NPWM; i++){
+        int power = pwm_get_power(i);
 
-        uint8_t state = servo->power >= stage;
-
-        if(state != servo->last_state){
-            gpio_set_level(servo->gpio, 0, state);
-
-            servo->last_state = state;
-        }
+        power >= stage ? pwm_on(i) : pwm_off(i);
     }
 
     stage += 1;
@@ -48,20 +41,4 @@ static irq_return_t lx106_pwm_handle(int irq_nr, void *data){
         stage = 1;
     change_ccompare();
     return IRQ_HANDLED;
-}
-
-int lx106_pwm_add_servo(uint8_t idx, lx106_pwm_t servo){
-    if(idx >= MAX_SERVOS)
-        return -1;
-
-    servo.enabled = 1;
-    servo.gpio = gpio_by_num(servo.pin);
-    servos[idx] = servo;
-
-    gpio_settings(servo.gpio, 0, GPIO_MODE_OUTPUT);
-    return 0;
-}
-
-int lx106_pwm_remove_servo(uint8_t idx){
-    servos[idx].enabled = 0;
 }
