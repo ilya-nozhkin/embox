53532723fe7baa576425376df9a1ef4ad31d9bd1
Egor Shitov <liberaid2@gmail.com>
2017-07-26 19:39:45 +0300

    pwm: interface updated

--- src/drivers/pwm/pwm.c
+++ src/drivers/pwm/pwm.c
@@ -1,3 +1,11 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 21.07.2017
+ * @author Egor Shitov
+ */
+
 #include "pwm.h"
 
 #include <errno.h>
@@ -7,56 +15,58 @@
 
 INDEX_DEF(pwm_idx,0,NPWM);
 
-static struct gpio *gpios[NPWM];
+typedef struct pwm{
+	struct gpio *gpio;
+	int power;
+} pwm_t;
 static pwm_t pwms[NPWM];
 
 int pwm_add(uint8_t pin){
-    pwm_t pwm;
-    int idx;
+	pwm_t pwm;
+	int idx;
 
-    if((idx = index_alloc(&pwm_idx, INDEX_MIN)) < 0)
-        return -ENOMEM;
+	if((idx = index_alloc(&pwm_idx, INDEX_MIN)) < 0)
+		return -ENOMEM;
 
-    pwm.pin = pin;
-    pwm.power = 0;
+	pwm.gpio = gpio_by_num(pin);
+	pwm.power = 0;
 
-    pwms[idx] = pwm;
-    gpios[idx] = gpio_by_num(pin);
+	pwms[idx] = pwm;
 
-    gpio_settings(gpios[idx], 0, GPIO_MODE_OUTPUT);
+	gpio_settings(pwm.gpio, 0, GPIO_MODE_OUTPUT);
 
-    return idx;
+	return idx;
 }
 
 int pwm_remove(uint8_t idx){
-    if(idx >= NPWM)
-        return -1;
+	if(idx >= NPWM)
+		return -1;
 
-    index_free(&pwm_idx, idx);
-    pwms[idx].power = 0;
+	index_free(&pwm_idx, idx);
+	pwms[idx].power = 0;
 
-    return 0;
+	return 0;
 }
 
-int pwm_set_power(uint8_t idx, uint8_t power){
-    if(idx >= NPWM)
-        return -1;
+int pwm_set_power(uint8_t idx, int power){
+	if(idx >= NPWM)
+		return -1;
 
-    pwms[idx].power = power;
-    return 0;
+	pwms[idx].power = power;
+	return 0;
 }
 
 int pwm_get_power(uint8_t idx){
-    if(idx >= NPWM)
-        return -1;
+	if(idx >= NPWM)
+		return -1;
 
-    return pwms[idx].power;
+	return pwms[idx].power;
 }
 
 void pwm_on(uint8_t idx){
-    gpio_set_level(gpios[idx], 0, 1);
+	gpio_set_level(pwms[idx].gpio, 0, 1);
 }
 
 void pwm_off(uint8_t idx){
-    gpio_set_level(gpios[idx], 0, 0);
+	gpio_set_level(pwms[idx].gpio, 0, 0);
 }
--- src/drivers/pwm/pwm.h
+++ src/drivers/pwm/pwm.h
@@ -1,3 +1,11 @@
+/**
+ * @file
+ * @brief
+ *
+ * @date 21.07.2017
+ * @author Egor Shitov
+ */
+
 #ifndef DRIVERS_PWM_PWM_H
 #define DRIVERS_PWM_PWM_H
 
@@ -5,19 +13,28 @@
 
 #define NPWM 2
 
-typedef struct pwm{
-    uint8_t pin;
-    uint8_t power;
-} pwm_t;
+/* HOW TO IMPLEMENT THIS:
+ * 1. implement pwm_enable and pwm_disable (e.g. interrupts by timer on/off)
+ * 2. every iteration call pwm_get_power
+ * 3. if power is enough for this iteration call pwm_on over otherwise call pwm_off
+ * */
+
+/* HOW TO USE THIS:
+ * 1. add pwm using pwm_add
+ * 2. set pwm power via pwm_set_power
+ * 3. enable pwm via pwm_enable
+ * */
 
 /* depends on platform */
-int pwm_enable(void);
-int pwm_disable(void);
+int pwm_enable(void);	// returns 0 on success ?
+int pwm_disable(void);	// returns 0 on success ?
 
-/* platform-independent functions */
-int pwm_add(uint8_t pin);
-int pwm_remove(uint8_t idx);
-int pwm_set_power(uint8_t idx, uint8_t power);
+/* platform-independent functions (used to communicate with pwms) */
+int pwm_add(uint8_t pin);		// returns idx on success otherwise -ENOMEM
+int pwm_remove(uint8_t idx);	// returns 0 on success
+int pwm_set_power(uint8_t idx, int power);	// returns 0 on success
+
+/* these functions are called by platform-dependent code */
 int pwm_get_power(uint8_t idx);
 void pwm_on(uint8_t idx);
 void pwm_off(uint8_t idx);
